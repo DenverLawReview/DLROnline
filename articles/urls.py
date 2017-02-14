58abc88990c8de77479a0027711e6bb05916dfaa
Patrick Sweet <patmsweet@gmail.com>
2017-02-13 22:00:02 -0700

    changed footnotes

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ app_name = "articles"
 urlpatterns = [
     # Articles
     url(r'^articles/$', ArticleList.as_view(), name='article-list'),
-    url(r'^articles/(?P<pk>\d+)/(?P<slug>[-_\w]+)/$',
+    url(r'^articles/(?P<pk>\d+)/(?P<slug>[-_\w]+)$',
         ArticleDetail.as_view(), name='article-detail'),
     # Categories
     url(r'^articles/categories/$', CategoryList.as_view(), name='category-list'),
